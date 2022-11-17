c5882f3d783467257d1a6ab04f0eb2a54262adeb
Philipp Wendel <philippwendel@protonmail.com>
2022-11-17 20:12:29 +0100

    Blinky working

--- build.zig
+++ build.zig
@@ -2,20 +2,25 @@ const std = @import("std");
 const microzig = @import("microzig/src/main.zig");
 
 pub fn build(b: *std.build.Builder) !void {
+    const project_name: []const u8 = "NodeMCU-BU01";
+
     const backing = .{
         .chip = microzig.chips.stm32f103x8,
     };
 
     var exe = microzig.addEmbeddedExecutable(
         b,
-        "my-executable.bin",
+        project_name ++ ".elf",
         "src/main.zig",
         backing,
         .{
-          // optional slice of packages that can be imported into your app:
-          // .packages = &my_packages,
+            // optional slice of packages that can be imported into your app:
+            // .packages = &my_packages,
         },
     );
     exe.setBuildMode(.ReleaseSmall);
     exe.install();
+
+    const exe_bin = b.addInstallRaw(exe.inner, project_name ++ ".bin", .{});
+    b.getInstallStep().dependOn(&exe_bin.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -4,14 +4,26 @@ const micro = @import("microzig");
 
 const regs = micro.chip.registers;
 
+pub fn delay(ticks: i32) void {
+    var i = @divExact(ticks, 5);
+    while (i > 0) : (i -= 1) {
+        asm volatile ("nop");
+    }
+}
+
+const delay_time = 1000000;
+
 pub fn main() void {
     // Enable AHB clock
-    regs.RCC.APB2ENR.modify(.{.IOPAEN=1});
+    regs.RCC.APB2ENR.modify(.{ .IOPAEN = 1 });
 
     // GPIO port mode register
-    regs.GPIOA.CRL.modify(.{.MODE0=0b01,.MODE1=0b01});
-    regs.GPIOA.BSRR.modify(.{.BS1 = 1, .BR2 = 1});
+    regs.GPIOA.CRL.modify(.{ .MODE1 = 0b01, .CNF1 = 0b00, .MODE2 = 0b01, .CNF2 = 0b00 });
 
     while (true) {
+        delay(delay_time);
+        regs.GPIOA.BSRR.modify(.{ .BS1 = 1, .BR2 = 1 });
+        delay(delay_time);
+        regs.GPIOA.BSRR.modify(.{ .BR1 = 1, .BS2 = 1 });
     }
 }
