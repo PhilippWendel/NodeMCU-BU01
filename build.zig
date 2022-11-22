fae0756c3f1f9683c0a18047520e8e82cd1a3c1e
Philipp Wendel <philippwendel@protonmail.com>
2022-11-22 18:28:19 +0100

    Added tag and anchor

--- build.zig
+++ build.zig
@@ -4,23 +4,57 @@ const microzig = @import("microzig/src/main.zig");
 pub fn build(b: *std.build.Builder) !void {
     const project_name: []const u8 = "NodeMCU-BU01";
 
-    const backing = .{
-        .chip = microzig.chips.stm32f103x8,
-    };
+    const nodeType = enum { tag, anchor };
 
-    var exe = microzig.addEmbeddedExecutable(
+    const tag_options = b.addOptions();
+    tag_options.addOption(nodeType, "nodeType", nodeType.tag);
+
+    var tag = microzig.addEmbeddedExecutable(
         b,
-        project_name ++ ".elf",
+        project_name ++ "_tag.elf",
         "src/main.zig",
-        backing,
-        .{
-            // optional slice of packages that can be imported into your app:
-            // .packages = &my_packages,
-        },
+        .{ .chip = microzig.chips.stm32f103x8 },
+        .{},
     );
-    exe.setBuildMode(.ReleaseSmall);
-    exe.install();
+    tag.addOptions("build_options", tag_options);
+    tag.setBuildMode(.ReleaseSmall);
+    tag.install();
 
-    const exe_bin = b.addInstallRaw(exe.inner, project_name ++ ".bin", .{});
-    b.getInstallStep().dependOn(&exe_bin.step);
+    // Convert elf file to binary file
+    const tag_bin = b.addInstallRaw(tag.inner, project_name ++ "_tag.bin", .{});
+    b.getInstallStep().dependOn(&tag_bin.step);
+
+    const anchor_options = b.addOptions();
+    anchor_options.addOption(nodeType, "nodeType", nodeType.anchor);
+
+    var anchor = microzig.addEmbeddedExecutable(
+        b,
+        project_name ++ "_anchor.elf",
+        "src/main.zig",
+        .{ .chip = microzig.chips.stm32f103x8 },
+        .{},
+    );
+    anchor.addOptions("build_options", anchor_options);
+    anchor.setBuildMode(.ReleaseSmall);
+    anchor.install();
+
+    // Convert elf file to binary file
+    const anchor_bin = b.addInstallRaw(anchor.inner, project_name ++ "_anchor.bin", .{});
+    b.getInstallStep().dependOn(&anchor_bin.step);
+
+    // Read unique id
+    // const read_unique_id_cmd = b.addSystemCommand(&[_][]const u8{
+    //     "st-flash",
+    //     "read",
+    //     "id", // File to save unique id to
+    //     "0x1FFFF7E8", // Base address of unique id
+    //     "0xC", // Length of unique id
+    // });
+
+    // const upload_cmd = b.addSystemCommand(&[_][]const u8{ "st-flash", "--reset", "write", b.getInstallPath(exe_bin.dest_dir, exe_bin.dest_filename), "0x08000000" });
+    // upload_cmd.step.dependOn(b.getInstallStep());
+
+    // const upload_step = b.step("upload", "Upload binary to microcontroller");
+    // upload_step.dependOn(&read_unique_id_cmd.step);
+    // upload_step.dependOn(&upload_cmd.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,9 @@
 // https://embetronicx.com/tutorials/microcontrollers/stm32/stm32-gpio-tutorial/#STM32_GPIO_Tutorial_-_Registers_used_in_STM32_GPIO
 
 const micro = @import("microzig");
-
 const regs = micro.chip.registers;
+
+const build_options = @import("build_options");
 
 pub fn delay(ticks: i32) void {
     var i = @divExact(ticks, 5);
@@ -17,13 +18,23 @@ pub fn main() void {
     // Enable AHB clock
     regs.RCC.APB2ENR.modify(.{ .IOPAEN = 1 });
 
-    // GPIO port mode register
-    regs.GPIOA.CRL.modify(.{ .MODE1 = 0b01, .CNF1 = 0b00, .MODE2 = 0b01, .CNF2 = 0b00 });
+    // GPIO port mode
+
+    regs.GPIOA.CRL.modify(switch (build_options.nodeType) {
+        .tag => .{ .MODE1 = 0b01, .CNF1 = 0b00 },
+        .anchor => .{ .MODE2 = 0b01, .CNF2 = 0b00 },
+    });
 
     while (true) {
         delay(delay_time);
-        regs.GPIOA.BSRR.modify(.{ .BS1 = 1, .BR2 = 1 });
+        regs.GPIOA.BSRR.modify(switch (build_options.nodeType) {
+            .tag => .{ .BS1 = 1 },
+            .anchor => .{ .BS2 = 1 },
+        });
         delay(delay_time);
-        regs.GPIOA.BSRR.modify(.{ .BR1 = 1, .BS2 = 1 });
+        regs.GPIOA.BSRR.modify(switch (build_options.nodeType) {
+            .tag => .{ .BR1 = 1 },
+            .anchor => .{ .BR2 = 1 },
+        });
     }
 }
